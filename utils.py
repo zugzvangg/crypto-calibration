e8d19006739bbae4e4f4b23122368138f0d0b2d9
Dmitriy Bazanov <zugzvangg1@gmail.com>
2023-01-15 16:52:14 +0300

    feat: Add function to calculate years to exp date

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ from loguru import logger
 import datetime
 nest_asyncio.apply()
 import warnings
+from typing import Union
 
 warnings.filterwarnings("ignore")
 
@@ -108,3 +109,6 @@ def get_human_timestamp(timestamp: int):
     """Get human-readable timestamp from linux date"""
     return datetime.datetime.fromtimestamp(timestamp/1000000.0).strftime('%Y-%m-%d %H:%M:%S.%f')
 
+def get_difference_between_now_and_expirety_date(now: int, expiration_date: int):
+    """Returns the time between now and expiration date in YEARS"""
+    return (now - expiration_date)/(1_000_000*60*60*24*365)
