a815ad43d5abcf56ea17ad6b973dc451f6352815
Dmitriy Bazanov <zugzvangg1@gmail.com>
2023-03-26 23:21:13 +0300

    Add wirshert to finding heston params

--- src/Wishart/wishart.py
+++ src/Wishart/wishart.py
@@ -5,10 +5,11 @@ import pandas as pd
 from src.utils import get_tick, get_implied_volatility, get_bid_ask
 from src.levenberg_marquardt import LevenbergMarquardt
 from typing import Union
-import warnings 
+from scipy.integrate import quad
+import warnings
+
 warnings.filterwarnings("ignore")
-from Heston.heston import JacHes
-from Heston.heston import ModelParameters as HestonModelParameters
+from ..Heston.heston import ModelParameters as HestonModelParameters, JacHes, MarketParameters as HestonMarketParameters
 
 
 _spec_market_params = [
@@ -22,11 +23,11 @@ _spec_market_params = [
 ]
 
 _spec_model_params = [
-    ("beta", nb.float64), 
+    ("beta", nb.float64),
     ("SIGMA", nb.types.Array(nb.float64, 2, "A")),
     ("M", nb.types.Array(nb.float64, 2, "A")),
     ("Q", nb.types.Array(nb.float64, 2, "A")),
-    ("alpha", nb.float64)
+    ("alpha", nb.float64),
 ]
 
 
@@ -71,16 +72,16 @@ class WirsherModelParameters(object):
         self,
         beta: nb.float64,
         SIGMA: nb.types.Array(nb.float64, 2, "A"),
-        R: nb.types.Array(nb.float64, 2, "A"),
+        M: nb.types.Array(nb.float64, 2, "A"),
         Q: nb.types.Array(nb.float64, 2, "A"),
-        alpha: nb.float64
+        alpha: nb.float64,
     ):
         self.beta = beta
         self.SIGMA = SIGMA
-        self.R = R
         self.Q = Q
+        self.M = M
         self.alpha = alpha
-        self.R = alpha*np.eye(2)
+        # self.R = alpha*np.eye(2)
 
 
 _tmp_values_get_iv_wishart = {}
@@ -140,9 +141,7 @@ def get_iv_wishart(market: MarketParameters, model: WirsherModelParameters):
                 + np.trace(
                     np.dot(
                         R,
-                        np.dot(
-                            Q, np.dot(np.dot(Q_T, R_T) + np.dot(R, Q), SIGMA)
-                        ),
+                        np.dot(Q, np.dot(np.dot(Q_T, R_T) + np.dot(R, Q), SIGMA)),
                     )
                 )
                 / 3
@@ -153,26 +152,116 @@ def get_iv_wishart(market: MarketParameters, model: WirsherModelParameters):
     return sigmas
 
 
-
 def Gamma(model: WirsherModelParameters, market: MarketParameters):
     # page 5 from orig article
     tau = market.T
     SIGMA = model.SIGMA
     M = model.M
-    L1 = np.exp(tau*M)
-    L2 = np.exp(tau*M.T)
+    L1 = np.exp(tau * M)
+    L2 = np.exp(tau * M.T)
     return np.dot(L1, np.dot(SIGMA, L2))
+
 
 def Theta(model: WirsherModelParameters, market: MarketParameters):
     Q = model.Q
     M = model.M
     Q11, Q12, Q21, Q22 = Q[0][0], Q[0][1], Q[1][0], Q[1][1]
     M11, M12, M21, M22 = M[0][0], M[0][1], M[1][0], M[1][1]
+    tau = market.T
+
+    # can't avoid scipy here
+    def M11_calc(u):
+        return (
+            Q11 * (Q11 * np.exp(M11 * u) + Q12 * np.exp(M12 * u))
+            + Q21 * (Q21 * np.exp(M11 * u) + Q22 * np.exp(M12 * u))
+        ) * np.exp(M11 * u) + (
+            Q12 * (Q11 * np.exp(M11 * u) + Q12 * np.exp(M12 * u))
+            + Q22 * (Q21 * np.exp(M11 * u) + Q22 * np.exp(M12 * u))
+        ) * np.exp(
+            M12 * u
+        )
+
+    def M12_calc(u):
+        return (
+            Q11 * (Q11 * np.exp(M11 * u) + Q12 * np.exp(M12 * u))
+            + Q21 * (Q21 * np.exp(M11 * u) + Q22 * np.exp(M12 * u))
+        ) * np.exp(M21 * u) + (
+            Q12 * (Q11 * np.exp(M11 * u) + Q12 * np.exp(M12 * u))
+            + Q22 * (Q21 * np.exp(M11 * u) + Q22 * np.exp(M12 * u))
+        ) * np.exp(
+            M22 * u
+        )
+
+    def M21_calc(u):
+        return (
+            Q11 * (Q11 * np.exp(M21 * u) + Q12 * np.exp(M22 * u))
+            + Q21 * (Q21 * np.exp(M21 * u) + Q22 * np.exp(M22 * u))
+        ) * np.exp(M11 * u) + (
+            Q12 * (Q11 * np.exp(M21 * u) + Q12 * np.exp(M22 * u))
+            + Q22 * (Q21 * np.exp(M21 * u) + Q22 * np.exp(M22 * u))
+        ) * np.exp(
+            M12 * u
+        )
+
+    def M22_calc(u):
+        return (
+            Q11 * (Q11 * np.exp(M21 * u) + Q12 * np.exp(M22 * u))
+            + Q21 * (Q21 * np.exp(M21 * u) + Q22 * np.exp(M22 * u))
+        ) * np.exp(M21 * u) + (
+            Q12 * (Q11 * np.exp(M21 * u) + Q12 * np.exp(M22 * u))
+            + Q22 * (Q21 * np.exp(M21 * u) + Q22 * np.exp(M22 * u))
+        ) * np.exp(
+            M22 * u
+        )
+
+    theta = np.array(
+        [
+            [quad(M11_calc, 0.0, tau)[0], quad(M12_calc, 0.0, tau)[0]],
+            [quad(M21_calc, 0.0, tau)[0], quad(M22_calc, 0.0, tau)[0]],
+        ],
+        dtype=np.float64,
+    )
+    return theta
 
 
+def E_nu_sigma(gamma: float, theta: float, model: WirsherModelParameters):
+    return np.trace(gamma + model.beta * theta)
 
+
+def VAR_nu_sigma(gamma: float, theta: float, model: WirsherModelParameters):
+    return 2 * np.trace(np.dot((2 * gamma + model.beta * theta), theta))
 
 
 def jacobian_wishart(market: MarketParameters, model: WirsherModelParameters):
-    heston_params = HestonModelParameters()
-    JacHes
+    # heston_params = HestonModelParameters()
+    # JacHes
+    gamma = Gamma(model=model, market=market)
+    theta = Theta(model=model, market=market)
+    matoj = E_nu_sigma(gamma, theta, model)
+    var = VAR_nu_sigma(gamma, theta, model)
+    # find heston params
+    beta, SIGMA, Q, M, alpha = model.beta, model.SIGMA, model.Q, model.M, model.alpha
+    R = alpha * np.eye(2)
+    eps = 0.1
+    V0 = np.clip(np.trace(SIGMA), eps, 10.0)
+    theta = np.clip(-beta / 2 * np.trace(np.dot(np.linalg.inv(M), np.dot(Q.T, Q))), eps, 10.0)
+    # NOTE: V0 - theta а точно тут V0? Там должно быть NU(t)
+    kappa = np.clip(-1 / market.T * np.log((matoj - theta) / (V0 - theta)), eps, 10.0)
+    eta = np.clip(np.sqrt(var / (alpha * kappa * ((1 - alpha) * V0 + alpha / 2 * theta))), eps, 100.0)
+    rho_num = np.trace(np.dot(R, np.dot(Q, SIGMA)))
+    rho_div = np.sqrt(np.trace(SIGMA)) * np.sqrt(
+        np.trace(np.dot(Q.T, np.dot(Q, SIGMA)))
+    )
+    rho = np.clip(rho_num / rho_div, -1.0 + eps, 1.0 - eps)
+    heston_params = HestonModelParameters(v0=V0, a=kappa, b=theta, c=eta, rho=rho)
+    print(V0, kappa, theta, eta, rho)
+    heston_market = HestonMarketParameters(S = market.S,
+                                           r = market.r,
+                                           T = market.T*np.ones(len(market.K)), 
+                                           C = market.C, 
+                                           types = market.types,
+                                           K = market.K
+                                           )
+    heston_jacobians = JacHes(model_parameters=heston_params, market_parameters=heston_market)
+    print(heston_jacobians)
+    # NOTE: look for mistake in theta
